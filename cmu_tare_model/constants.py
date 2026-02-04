6552fe84b3e99c8dd84215eb83f62a18646bdf4b
Jordan Malique Joseph <jordanjo@alumni.cmu.edu>
2026-02-04 16:46:55 -0500

    Update legacy code file paths to run v2_1 and compare results to v2_2

--- cmu_tare_model/constants.py
+++ cmu_tare_model/constants.py
@@ -27,34 +27,34 @@ ALLOWED_TECHNOLOGIES = {
         'Natural Gas Fuel Boiler', 'Natural Gas Fuel Furnace',
         'Propane Fuel Boiler', 'Propane Fuel Furnace'
     ],
-    # in.hvac_cooling_type exclude existing heat pump options
-    'cooling': [
-        'Central AC', 'Room AC'
+    # # in.hvac_cooling_type exclude existing heat pump options
+    # 'cooling': [
+    #     'Central AC', 'Room AC'
+    # ],
+    # in.water_heater_efficiency exclude heat pump options, tankless, other fuel (e.g., solar), and indirect fuel oil
+    'waterHeating': [
+        'Electric Premium', 'Electric Standard',
+        'Fuel Oil Premium', 'Fuel Oil Standard', 
+        'Natural Gas Premium', 'Natural Gas Standard',
+        'Propane Premium', 'Propane Standard'
     ],
-    # # in.water_heater_efficiency exclude heat pump options, tankless, other fuel (e.g., solar), and indirect fuel oil
-    # 'waterHeating': [
-    #     'Electric Premium', 'Electric Standard',
-    #     'Fuel Oil Premium', 'Fuel Oil Standard', 
-    #     'Natural Gas Premium', 'Natural Gas Standard',
-    #     'Propane Premium', 'Propane Standard'
-    # ],
-    # # in.clothes_dryer also includes information on usage (%) exclude homes with no dryer or heat pump dryers
-    # 'clothesDrying': [
-    #     'Electric', 'Gas', 'Propane'
-    # ],
-    # # in.cooking_range exclude electric cooking (since resistance ranges are an upgrade option) and homes with no cooking range
-    # 'cooking': [
-    #     'Gas', 'Propane'
-    # ]
+    # in.clothes_dryer also includes information on usage (%) exclude homes with no dryer or heat pump dryers
+    'clothesDrying': [
+        'Electric', 'Gas', 'Propane'
+    ],
+    # in.cooking_range exclude electric cooking (since resistance ranges are an upgrade option) and homes with no cooking range
+    'cooking': [
+        'Gas', 'Propane'
+    ]
 } 
 
 # POSSIBLY UPDATE THESE VALUES BASED ON NEW DATA FROM REMBD 2024
 EQUIPMENT_SPECS = {
     'heating': 15,
     # 'cooling': 15,
-    # 'waterHeating': 12,
-    # 'clothesDrying': 13,
-    # 'cooking': 15
+    'waterHeating': 12,
+    'clothesDrying': 13,
+    'cooking': 15
     }
 
 VALID_MENU_MPS = [0, 3, 4, 7, 8, 9, 10]
@@ -109,9 +109,9 @@ AMI_THRESHOLD = 150              # AMI percentage at which minimum rate applies
 UPGRADE_COLUMNS = {
     'heating': 'upgrade_hvac_heating_efficiency',
     # 'cooling': 'upgrade_hvac_cooling_efficiency',
-    # 'waterHeating': 'upgrade_water_heater_efficiency',
-    # 'clothesDrying': 'upgrade_clothes_dryer',
-    # 'cooking': 'upgrade_cooking_range'
+    'waterHeating': 'upgrade_water_heater_efficiency',
+    'clothesDrying': 'upgrade_clothes_dryer',
+    'cooking': 'upgrade_cooking_range'
     }
 
 # Mapping for categories and their corresponding rebate amounts
@@ -119,7 +119,7 @@ REBATE_MAPPING = {
     # Be sure to update code logic so that the space conditioning rebate is only applied once (i.e., $8000 total, not $8000 per equipment if both heating and cooling are upgraded)
     'heating': ('upgrade_hvac_heating_efficiency', ['ASHP', 'MSHP'], 8000.00),
     # 'cooling': ('upgrade_hvac_cooling_efficiency', ['Heat Pump'], 0.00),
-    # 'waterHeating': ('upgrade_water_heater_efficiency', ['Electric Heat Pump'], 1750.00),
-    # 'clothesDrying': ('upgrade_clothes_dryer', ['Electric, Premium, Heat Pump, Ventless'], 840.00),
-    # 'cooking': ('upgrade_cooking_range', ['Electric, '], 840.00)
+    'waterHeating': ('upgrade_water_heater_efficiency', ['Electric Heat Pump'], 1750.00),
+    'clothesDrying': ('upgrade_clothes_dryer', ['Electric, Premium, Heat Pump, Ventless'], 840.00),
+    'cooking': ('upgrade_cooking_range', ['Electric, '], 840.00)
 }
