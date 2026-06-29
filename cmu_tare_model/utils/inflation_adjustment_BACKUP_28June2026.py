03c038b0a8db81809cd0b56f5486c66ee65ca5ac
Jordan Malique Joseph <jordanjo@alumni.cmu.edu>
2026-06-28 22:57:52 -0400

    Deleted old archived files and cleaned up project directory. Moved all public health impact related code to a DEPRECATED_ folder to make it clear that is being retired. Purposefully, did not update the file paths so code will break where the old function calls and imports need to be removed.

--- cmu_tare_model/utils/inflation_adjustment_BACKUP_28June2026.py
+++ cmu_tare_model/utils/inflation_adjustment_BACKUP_28June2026.py
@@ -16,7 +16,7 @@ from config import PROJECT_ROOT
 # ==============================================================================================================================================================================
 
 # C:\Users\14128\Research\cmu-tare-model\cmu_tare_model\data\inflation_data\bls_cpiu_2005-2023.xlsx# Load the BLS Inflation Data
-filename = 'bls_cpiu_2005-2025.xlsx'
+filename = 'bls_cpiu_2005-2023.xlsx'
 relative_path = os.path.join("cmu_tare_model", "data", "inflation_data", filename)
 file_path = os.path.join(PROJECT_ROOT, relative_path)
 
@@ -33,16 +33,25 @@ df_bls_cpiu = pd.DataFrame({
 print(df_bls_cpiu)
 
 # Obtain the Annual CPIU values for the years of interest
+bls_cpi_annual_2006 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2006)].item()
 bls_cpi_annual_2008 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2008)].item()
+bls_cpi_annual_2010 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2010)].item()
+bls_cpi_annual_2013 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2013)].item()
 bls_cpi_annual_2018 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2018)].item()
+bls_cpi_annual_2019 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2019)].item()
 bls_cpi_annual_2020 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2020)].item()
+bls_cpi_annual_2021 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2021)].item()
+bls_cpi_annual_2022 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2022)].item()
 bls_cpi_annual_2023 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2023)].item()
-bls_cpi_annual_2024 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2024)].item()
-bls_cpi_annual_2025 = df_bls_cpiu['cpiu_annual'].loc[(df_bls_cpiu['year'] == 2025)].item()
 
 # Precompute constant values
-cpi_ratio_2025_2024 = bls_cpi_annual_2025 / bls_cpi_annual_2024  # For adjusting the income data from USD 2024 to USD 2025
-cpi_ratio_2025_2023 = bls_cpi_annual_2025 / bls_cpi_annual_2023  # For REMDB Capital Costs inflation adjustment from USD 2023 to USD 2025
-cpi_ratio_2025_2020 = bls_cpi_annual_2025 / bls_cpi_annual_2020  # For SCC (reported in USD 2020) inflation adjustment to USD 2025
-cpi_ratio_2025_2018 = bls_cpi_annual_2025 / bls_cpi_annual_2018  # ResStock is circa 2018, so assume that the costs are in USD 2018 and need to be inflated to USD 2025
-cpi_ratio_2025_2008 = bls_cpi_annual_2025 / bls_cpi_annual_2008  # For EPA VSL and SCC
+cpi_ratio_2023_2023 = bls_cpi_annual_2023 / bls_cpi_annual_2023
+cpi_ratio_2023_2022 = bls_cpi_annual_2023 / bls_cpi_annual_2022
+cpi_ratio_2023_2021 = bls_cpi_annual_2023 / bls_cpi_annual_2021  # For EPA VSL (11.3M USD-2021)
+cpi_ratio_2023_2020 = bls_cpi_annual_2023 / bls_cpi_annual_2020  # For SCC
+cpi_ratio_2023_2019 = bls_cpi_annual_2023 / bls_cpi_annual_2019 
+cpi_ratio_2023_2018 = bls_cpi_annual_2023 / bls_cpi_annual_2018 
+cpi_ratio_2023_2013 = bls_cpi_annual_2023 / bls_cpi_annual_2013
+cpi_ratio_2023_2010 = bls_cpi_annual_2023 / bls_cpi_annual_2010
+cpi_ratio_2023_2008 = bls_cpi_annual_2023 / bls_cpi_annual_2008  # For EPA VSL and SCC
+cpi_ratio_2023_2006 = bls_cpi_annual_2023 / bls_cpi_annual_2006  # For CACES RCM Marginal Social Costs 
